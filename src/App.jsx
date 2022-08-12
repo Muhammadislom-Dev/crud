3327027464726ba6ac684e088e98e82238d4c781
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-08-13 01:48:34 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import PostForm from './API/PostForm';
+import PostList from './API/PostList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       {/* <PostList /> */}
+       <PostForm />
+    </div>
+  );
+}
+
+export default App;
+
+
